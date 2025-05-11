fd79a8eeb37de919f15e8c865dbb5db5b0c04210
Kosmik <malpa.w.klapkach@gmail.com>
2025-05-12 01:15:48 +0200

    Finish ProceduralDisk example

--- Examples/Cylinder/ProceduralDisk.cs
+++ Examples/Cylinder/ProceduralDisk.cs
@@ -13,7 +13,32 @@ namespace Bipolar.ProceduralMeshes.Examples
 
         public override void BuildMesh(Mesh mesh)
         {
+            var vertices = new Vector3[Resolution + 1];
+            vertices[0] = Vector3.zero;
+            var triangles = new int[Resolution * 3];
+            int currentTriangleIndex = 0;   
 
+            float angleDelta = 360f / Resolution;
+            for (int i = 0; i < Resolution; i++)
+            {
+                int index = i + 1;
+                float angle = angleDelta * i;
+                Vector3 direction = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward;
+                vertices[index] = direction * Radius;
+                int previousIndex = i == 0 ? Resolution : i;
+                AddTriangle(0, previousIndex, index);
+            }
+
+            void AddTriangle(int a, int b, int c)
+            {
+                triangles[currentTriangleIndex++] = a;
+                triangles[currentTriangleIndex++] = b;
+                triangles[currentTriangleIndex++] = c;
+            }
+
+            mesh.Clear();
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
         }
     }
 }
